dba9b723f165c75ed12cc9e33eb53705dbe42dc2
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-02-15 01:55:00 +0100

    fix: memset

--- lib/minilib/memset.asm
+++ lib/minilib/memset.asm
@@ -14,12 +14,16 @@ memset:
     push rdi                  ; On sauvegarde rdi
     push rsi                  ; On sauvegarde rsi
     push rdx                  ; On sauvegarde rdx
+    cmp rdx, 0                ; On compare rdx à 0
+    je memset_end             ; Si rdx est égal à 0, on sort de la fonction
 
 memset_loop:
     mov byte [rdi], sil       ; On copie sil dans rdi
     inc rdi                   ; On incrémente rdi
     dec rdx                   ; On décrémente rdx
     jnz memset_loop           ; On boucle tant que rdx n'est pas nul
+
+memset_end:
     pop rdx                   ; On restaure rdx
     pop rsi                   ; On restaure rsi
     pop rdi                   ; On restaure rdi
