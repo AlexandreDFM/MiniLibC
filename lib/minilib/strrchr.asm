8c03a99d0ff2f5849f42edd8952f2d477537d37a
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-02-20 09:18:52 +0100

    fix: strrchr

--- lib/minilib/strrchr.asm
+++ lib/minilib/strrchr.asm
@@ -16,12 +16,14 @@ strrchr:
     mov rax, 0                ; On met NULL dans rax
     cmp rdi, 0                ; On compare rdi avec 0 (NULL)
     je strrchr_end            ; Si c'est NULL, on sort de la fonction
-
-strrchr_loop:
     cmp byte [rdi], sil       ; Compare rdi avec rsi
     je strrchr_set            ; Si c'est égal, on met rdi dans rax
+
+strrchr_loop:
     cmp byte [rdi], 0         ; Compare le byte à l'adresse rdi avec 0 (NULL)
     je strrchr_end            ; Si c'est 0, on sort de la boucle
+    cmp byte [rdi], sil       ; Compare rdi avec rsi
+    je strrchr_set            ; Si c'est égal, on met rdi dans rax
     inc rdi                   ; On incrémente rdi
     jmp strrchr_loop          ; On boucle
 
