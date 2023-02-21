7fdc26b318dff69e526ccbb2f9894075f02421c8
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-02-21 23:47:59 +0100

    fix: memmove

--- lib/minilib/memmove.asm
+++ lib/minilib/memmove.asm
@@ -12,20 +12,29 @@ memmove:
     push rbp                      ; Prologue
     mov rbp, rsp                  ; Stack Frame Setup
     xor rcx, rcx                  ; On initialise rcx à 0
+    xor r8, r8                    ; On initialise r8 à 0
     cmp rdi, 0                    ; On compare rdi à 0
-    je memmove_loop_end           ; Si rdi = 0, on sort de la boucle
+    je memmove_end                ; Si rdi = 0, on sort de la boucle
     cmp rsi, 0                    ; On compare rsi à 0
-    je memmove_loop_end           ; Si rsi = 0, on sort de la boucle
+    je memmove_end                ; Si rsi = 0, on sort de la boucle
 
 memmove_loop:
-    cmp rdx, rcx                  ; On compare rdx à 0
-    je memmove_loop_end           ; Si rdx = 0, on sort de la boucle
-    mov al, byte [rsi + rcx]      ; On copie le contenu de rsi dans al
-    mov byte [rdi + rcx], al      ; On copie le contenu de al dans rdi
+    cmp rdx, rcx                  ; On compare rdx à rcx
+    je memmove_loop_end           ; Si rdx = rcx, on sort de la boucle
+    mov r8b, byte [rsi + rcx]     ; On copie le contenu de r8 dans al
+    push r8                       ; On empile r8
     inc rcx                       ; On incrémente rcx
     jmp memmove_loop              ; On boucle
 
 memmove_loop_end:
+    dec rcx                       ; On décrémente rcx
+    pop r8                        ; On dépile r8
+    mov byte [rdi + rcx], r8b     ; On copie le contenu de r8 dans rdi
+    cmp rcx, 0                    ; On compare rcx à 0
+    je memmove_end                ; Si rdx = 0, on sort de la boucle
+    jmp memmove_loop_end          ; On boucle
+
+memmove_end:
     mov rax, rdi                  ; On copie rdi dans rax
     mov rbp, rsp                  ; Stack Frame Cleanup
     pop rbp                       ; Epilogue
