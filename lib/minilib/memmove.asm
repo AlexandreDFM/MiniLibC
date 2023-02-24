948ac3a1767de2eaff9c4c92c065b14a36ec10d5
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-02-24 03:50:28 +0100

    fix: memmove

--- lib/minilib/memmove.asm
+++ lib/minilib/memmove.asm
@@ -9,33 +9,38 @@ bits 64
 global memmove
 
 memmove:
-    push rbp                      ; Prologue
-    mov rbp, rsp                  ; Stack Frame Setup
-    xor rcx, rcx                  ; On initialise rcx à 0
-    xor r8, r8                    ; On initialise r8 à 0
-    cmp rdi, 0                    ; On compare rdi à 0
-    je memmove_end                ; Si rdi = 0, on sort de la boucle
-    cmp rsi, 0                    ; On compare rsi à 0
-    je memmove_end                ; Si rsi = 0, on sort de la boucle
+    push rbp                    ; Prologue
+    mov rbp, rsp                ; Stack Frame Setup
+    xor r8, r8                  ; On initialise r8 à 0
+    mov rax, rdi                ; On copie rdi dans rax
+    cmp rdi, rsi                ; On compare rdi et rsi
+    je memmove_end              ; Si rdi = rsi, on sort de la boucle
+    jb memmove_bforward         ; Si rdi < rsi, on va copier de gauche à droite
+    mov rcx, rsi                ; On copie rsi dans rcx
+    add rcx, rdx                ; On ajoute rdx à rcx
+    cmp rdi, rcx                ; On compare rdi et rcx
+    jae memmove_bforward        ; Si rdi >= rcx, on va copier de gauche à droite
 
-memmove_loop:
-    cmp rdx, rcx                  ; On compare rdx à rcx
-    je memmove_loop_end           ; Si rdx = rcx, on sort de la boucle
-    mov r8b, byte [rsi + rcx]     ; On copie le contenu de r8 dans al
-    push r8                       ; On empile r8
-    inc rcx                       ; On incrémente rcx
-    jmp memmove_loop              ; On boucle
+memmove_backward:
+    cmp rdx, 0                  ; On compare rdx à 0
+    je memmove_end              ; Si rdx = 0, on sort de la boucle
+    dec rdx                     ; On décrémente rcx
+    mov r8b, byte [rsi + rdx]   ; On copie le contenu de rsi dans r8b
+    mov byte [rdi + rdx], r8b   ; On copie le contenu de r8b dans rdi
+    jmp memmove_backward        ; On boucle
 
-memmove_loop_end:
-    dec rcx                       ; On décrémente rcx
-    pop r8                        ; On dépile r8
-    mov byte [rdi + rcx], r8b     ; On copie le contenu de r8 dans rdi
-    cmp rcx, 0                    ; On compare rcx à 0
-    je memmove_end                ; Si rdx = 0, on sort de la boucle
-    jmp memmove_loop_end          ; On boucle
+memmove_bforward:
+    xor rcx, rcx                ; On initialise rcx à 0
+
+memmove_forward:
+    cmp rdx, rcx                ; On compare rdx à 0
+    je memmove_end              ; Si rdx = 0, on sort de la boucle
+    mov r8b, byte [rsi + rcx]   ; On copie le contenu de rsi dans al
+    mov byte [rdi + rcx], r8b   ; On copie le contenu de al dans rdi
+    inc rcx                     ; On incrémente rcx
+    jmp memmove_forward         ; On boucle
 
 memmove_end:
-    mov rax, rdi                  ; On copie rdi dans rax
-    mov rbp, rsp                  ; Stack Frame Cleanup
-    pop rbp                       ; Epilogue
-    ret                           ; Return
+    mov rbp, rsp                ; Stack Frame Cleanup
+    pop rbp                     ; Epilogue
+    ret                         ; On retourne
